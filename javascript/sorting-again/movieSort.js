e0c4a39441d44f96223d8f0a9731ade3e2f4b88d
Lacey Washington <lacey.m.washington@gmail.com>
2023-10-02 23:41:22 -0500

    testing only one test isnt passing and it is annoying

---
+++ javascript/sorting-again/movieSort.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const data = require('./db.json');
+
+
+function sortByRecentYear(movies) {
+    return movies.sort((a, b) => b.year - a.year);
+}
+
+function sortByTitle(movies) {
+    return movies.sort((a, b) => {
+        const titleA = a.title.replace(/^(a|an|the) /i, '');
+        const titleB = b.title.replace(/^(a|an|the) /i, '');
+        return titleA.localeCompare(titleB);
+    });
+}
+
+let sortedByYear = sortByRecentYear(data.movies).slice(0, 10);
+let sortedByTitle = sortByTitle(data.movies).slice(0, 10);
+
+
+module.exports = {
+    sortByRecentYear,
+    sortByTitle,
+    sortedByTitle,
+    sortedByYear
+};
